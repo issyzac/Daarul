fb7459269782923f40684e39b30be600affdf728
issyzac <issyzac.iz@gmail.com>
2015-07-02 14:00:39 +0300

    database changes

--- tea/db_connection.php
+++ tea/db_connection.php
@@ -1,4 +1,4 @@
 <?php
-$dbConnection = mysql_connect('localhost', 'root', '') or die(mysql_error());
+$dbConnection = mysql_connect('localhost', 'root', 'root') or die(mysql_error());
 $dbSelection = mysql_select_db('pmngdb', $dbConnection) or die(mysql_error());
 ?>
